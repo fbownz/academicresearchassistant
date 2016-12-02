caaa2ac33a872d38054a39754a568b2eb2918c6c
fbownz <bownzcash@gmail.com>
2016-12-02 16:21:02 +0300

    Updated fontAwesome cdn link n profile page'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" href="/css/plugins/datatables/dataTables.bootstrap.css">
   @endif
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -181,7 +181,7 @@
                       @endif
                       <div class="box-title">
                       <div class="box-header with-border"></div> 
-                          <h3 > <i class="fa fa-user margin-r-5"></i>More details about Me</h3>
+                          <h3 class="box-title" style="color: #3c8dbc" > <i class="fa fa-user margin-r-5"></i>More details about Me</h3>
                       </div>
                       <div class="box-body">
                         <span class="col-md-4">
