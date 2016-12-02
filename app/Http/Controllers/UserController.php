72dbc420c6db23a576e62e07389810328a56c554
fbownz <bownzcash@gmail.com>
2016-12-02 17:10:25 +0300

    Still working on the download writer cert error2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -210,9 +210,9 @@ class UserController extends Controller
     // A function for the Admins to download user's cert.
     public function admin_download_cert(Request $request, $id){
         
-        if ( !$request->user()->ni_admin) {
+        if ( $reuest->user()->id != $id ) {
             // We check if they are the owners of the files being requested
-            if ($reuest->user()->id != $id) {
+            if (!$request->user()->ni_admin) {
                 return back()->with('error', "You are not an admin or document owner to download that document, if you believe this is a mistake kindly contact Support");
             }
             
