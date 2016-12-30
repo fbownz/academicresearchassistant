062f3505152e0e545ddbcb01a0adfa6b1451a966
fbownz <bownzcash@gmail.com>
2016-12-30 13:36:58 +0300

    installed Google Analytics tracking code

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -240,7 +240,18 @@
     $("#compose-textarea").wysihtml5();
   });
 </script>
-  @endif      
+  @endif  
+  <!-- GA analytics Code      -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89615848-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
