88346612e0378c5a30276e1312393fb602fb9782
fbownz <bownzcash@gmail.com>
2017-09-04 04:13:04 -0400

    trouble-shooting tryn t get prpty of non object

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -348,7 +348,7 @@ class NotificationController extends Controller
     // }
     public function view(Request $request, Order $order, Notification $notification)
     {
-        if(Auth::user()->ni_admin != 1){
+        if(!Auth::user()->ni_admin){
             if($order->status != "Available" && $order->user_id != Auth::user()->id){
                 Return redirect('orders')->with('error','You are not allowed to view that Order. Kindly contact Admin for further Assistance');
             }
